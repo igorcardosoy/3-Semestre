3fdf0e0c3712f173fd2bfe9161513afded7fde42
Igor.Cardoso <igorfilipicardoso@gmail.com>
2024-03-26 15:46:51 -0300

    IFitness, app servlet

---
+++ WEB1/Eclipse/IFitness/src/main/java/br/edu/ifsp/arq/tsi/arqweb1/ifitness/model/util/user/UserReader.java
@@ -0,0 +1,12 @@
+package br.edu.ifsp.arq.tsi.arqweb1.ifitness.model.util.user;
+
+import java.util.List;
+import br.edu.ifsp.arq.tsi.arqweb1.ifitness.model.User;
+
+public class UserReader {
+
+	public static List<User> read() {
+		
+	}
+
+}
